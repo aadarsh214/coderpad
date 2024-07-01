b54bf28465c9b8f1947e1bacea3729afedc67af2
Poras-oss <55978642+Poras-oss@users.noreply.github.com>
2024-07-02 01:34:56 +0530

    added network conif

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // This will bind the server to 0.0.0.0, making it accessible on your network
+    port: 5173, // Default port
+  },
 })
