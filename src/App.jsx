35f02f70930c1215265ccc083e6263c569da0550
Poras-oss <55978642+Poras-oss@users.noreply.github.com>
2024-07-01 21:08:21 +0530

    added meta tags for https

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,15 @@ import Home from './components/Home';
 import QuizApp from './components/QuizApp'; // Your QuizApp component
 
 const App = () => {
+
+  useEffect(() => {
+    const metaTag = document.createElement('meta');
+    metaTag.httpEquiv = 'Content-Security-Policy';
+    metaTag.content = 'upgrade-insecure-requests';
+    document.getElementsByTagName('head')[0].appendChild(metaTag);
+  }, []);
+
+
   return (
     <BrowserRouter>
       <Routes>
