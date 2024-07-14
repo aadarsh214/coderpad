0c0b2e73037bc5e3a894265632b0cd6b39e1a236
Poras-oss <55978642+Poras-oss@users.noreply.github.com>
2024-07-14 17:14:38 +0530

    A good readable layout with working commit(ye recovery ke liye use hoga rollback karne)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,7 @@ import queryString from 'query-string';
 import { useNavigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 import Navbar from './Navbar';
+import '../App.css'
 
 const Home = () => {
   const { isAuthenticated, user } = useAuth0();
@@ -14,15 +15,13 @@ const Home = () => {
   const parsed = queryString.parse(window.location.search);
   const userID = parsed.userID;
 
- 
-
   useEffect(() => {
     fetchQuizzes();
   }, []);
 
   const removeQuizTypePrefix = (quizName) => {
-  return quizName.replace(/^(sql:|python:|mcq:)\s*/i, '');
-};
+    return quizName.replace(/^(sql:|python:|mcq:)\s*/i, '');
+  };
 
   const fetchQuizzes = async () => {
     try {
@@ -36,7 +35,7 @@ const Home = () => {
   const handleStartQuiz = (quizID, userID, quizName) => {
     if (!isAuthenticated) {
       alert('You need to log in to start the quiz.');
-      return; // Exit early if the user is not authenticated
+      return;
     }
   
     const lowerCaseQuizName = quizName.toLowerCase();
@@ -51,7 +50,6 @@ const Home = () => {
       alert('Unknown quiz type.');
     }
   };
-  
 
   const handleRegisterQuiz = async (quizID, userID) => {
     if (!isAuthenticated) {
@@ -97,7 +95,7 @@ const Home = () => {
     if (now < startDate) {
       return 'bg-blue-600 hover:bg-blue-700';
     } else if (now >= startDate && now <= endDate) {
-      return 'bg-home-button hover:bg-gray-800';
+      return 'bg-custom-blue  hover:bg-gray-800';
     } else {
       return 'bg-gray-700 hover:bg-gray-700';
     }
@@ -120,82 +118,75 @@ const Home = () => {
     <div>
       <Navbar />
       <div className="flex flex-col items-center">
-  <div className="my-6 bg-gray-100 p-3 rounded-lg shadow-md">
-    <button
-      onClick={() => setFilterType('all')}
-      className={`mx-1 px-5 py-2 rounded-md font-semibold transition-all duration-300 ease-in-out ${
-        filterType === 'all'
-          ? 'bg-blue-500 text-white shadow-md'
-          : 'bg-white text-gray-800 hover:bg-gray-200'
-      }`}
-    >
-      All
-    </button>
-    <button
-      onClick={() => setFilterType('sql')}
-      className={`mx-1 px-5 py-2 rounded-md font-semibold transition-all duration-300 ease-in-out ${
-        filterType === 'sql'
-          ? 'bg-green-500 text-white shadow-md'
-          : 'bg-white text-gray-800 hover:bg-gray-200'
-      }`}
-    >
-      SQL
-    </button>
-    <button
-      onClick={() => setFilterType('python')}
-      className={`mx-1 px-5 py-2 rounded-md font-semibold transition-all duration-300 ease-in-out ${
-        filterType === 'python'
-          ? 'bg-yellow-500 text-white shadow-md'
-          : 'bg-white text-gray-800 hover:bg-gray-200'
-      }`}
-    >
-      Python
-    </button>
-    <button
-      onClick={() => setFilterType('mcq')}
-      className={`mx-1 px-5 py-2 rounded-md font-semibold transition-all duration-300 ease-in-out ${
-        filterType === 'mcq'
-          ? 'bg-red-500 text-white shadow-md'
-          : 'bg-white text-gray-800 hover:bg-gray-200'
-      }`}
-    >
-      MCQ
-    </button>
-  </div>
-
-
-        <div className="flex flex-wrap justify-center">
-  {filteredQuizzes.map((quiz) => (
-    <div key={quiz._id} className="max-w-sm w-full sm:w-1/2 md:w-1/3 lg:w-1/4 rounded overflow-hidden shadow-xl m-4">
-      <div className="flex flex-col h-full">
-        <div className="px-6 py-4 border-[2px] flex-1">
-          <div className="text-xl mb-2 line-clamp-3">{removeQuizTypePrefix(quiz.quizName)}</div>
-          {/* <p className="text-gray-900 text-base">
-            Start Time: {new Date(quiz.start).toLocaleString()}<br />
-            End Time: {new Date(quiz.end).toLocaleString()}
-          </p> */}
-        </div>
-        <div className="px-6 py-4">
+        <div className="my-6 bg-gray-100 p-3 rounded-lg shadow-md">
           <button
-            onClick={() => {
-              if (determineButtonLabel(quiz) === 'Register') {
-                handleRegisterQuiz(quiz._id, userID);
-              } else if (determineButtonLabel(quiz) === 'Start') {
-                handleStartQuiz(quiz._id, userID, quiz.quizName);
-              }
-            }}
-            className={`text-white font-bold py-2 px-5 rounded-xl ${determineButtonColor(quiz)}`}
-            disabled={determineButtonLabel(quiz) === 'Ended'}
+            onClick={() => setFilterType('all')}
+            className={`mx-1 px-5 py-2 rounded-md font-semibold transition-all duration-300 ease-in-out glow-effect ${
+              filterType === 'all'
+                ? 'bg-blue-500 text-white shadow-md'
+                : 'bg-white text-gray-800 hover:bg-gray-200'
+            }`}
           >
-            {determineButtonLabel(quiz)}
+            All
+          </button>
+          <button
+            onClick={() => setFilterType('sql')}
+            className={`mx-1 px-5 py-2 rounded-md font-semibold transition-all duration-300 ease-in-out glow-effect ${
+              filterType === 'sql'
+                ? 'bg-blue-500  text-white shadow-md'
+                : 'bg-white text-gray-800 hover:bg-gray-200'
+            }`}
+          >
+            SQL
+          </button>
+          <button
+            onClick={() => setFilterType('python')}
+            className={`mx-1 px-5 py-2 rounded-md font-semibold transition-all duration-300 ease-in-out glow-effect ${
+              filterType === 'python'
+                ? 'bg-blue-500  text-white shadow-md'
+                : 'bg-white text-gray-800 hover:bg-gray-200'
+            }`}
+          >
+            Python
+          </button>
+          <button
+            onClick={() => setFilterType('mcq')}
+            className={`mx-1 px-5 py-2 rounded-md font-semibold transition-all duration-300 ease-in-out glow-effect ${
+              filterType === 'mcq'
+                ? 'bg-red-500 text-white shadow-md'
+                : 'bg-white text-gray-800 hover:bg-gray-200'
+            }`}
+          >
+            MCQ
           </button>
         </div>
-      </div>
-    </div>
-  ))}
-</div>
 
-
+        <div className="flex flex-wrap justify-center bg">
+          {filteredQuizzes.map((quiz) => (
+            <div key={quiz._id} className="max-w-sm w-full sm:w-1/2 md:w-1/3 lg:w-1/4 rounded overflow-hidden shadow-xl m-4">
+              <div className="flex flex-col h-full">
+                <div className="px-6 py-4 border-[2px] flex-1">
+                  <div className="text-xl mb-2 line-clamp-3">{removeQuizTypePrefix(quiz.quizName)}</div>
+                </div>
+                <div className="px-6 py-4">
+                  <button
+                    onClick={() => {
+                      if (determineButtonLabel(quiz) === 'Register') {
+                        handleRegisterQuiz(quiz._id, userID);
+                      } else if (determineButtonLabel(quiz) === 'Start') {
+                        handleStartQuiz(quiz._id, userID, quiz.quizName);
+                      }
+                    }}
+                    className={`text-white font-bold py-2 px-5 rounded-xl glow-effect ${determineButtonColor(quiz)}`}
+                    disabled={determineButtonLabel(quiz) === 'Ended'}
+                  >
+                    {determineButtonLabel(quiz)}
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
